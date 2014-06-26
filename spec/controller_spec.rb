c167ea5fe5b5d76584159f6cee9df3a430cb8722
Andrew Montague <apprentice@Maldives.local>
2014-06-27 10:49:29 +1200

    Adding the new action test with desciption

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
 
-#Change "MyController" to the appropriate name
-describe MyController do
-  
-  
-  
-  
+#Change "ItemController" to the appropriate name
+describe ItemController do
+
+  describe "The #new action" do
+    # Factoring out the GET :new HTTP request before each test
+    # The #new action might not need many tests to begin with
+    before(:each) do
+      get :new
+    end
+
+    # The #new action should create a new object, usually a new
+    # instance of a model class
+    it "creates a new object" do
+      expect(assigns(:object)).to be_a_new(Class)
+      # The assigns(:object) is calling the instance variable,
+      # @object in your controller. The test then asks, is it
+      # an instance of the 'Class'.
+    end
+
+
+  end
+
+
 end
