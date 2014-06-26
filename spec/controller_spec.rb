bbd80f3d8b0e93ea64b856e87048af82bb2aa0a1
Kendall Flutey <kendallflutey@me.com>
2014-06-27 10:36:42 +1200

    edit tests that hit the database for an item

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
-#Change "MyController" to the appropriate name
-describe MyController do
+#Change "ItemController" to the appropriate name
+describe ItemController do
   
-  
-  
+  #these tests will hit the database
+  #change "item" to the appropriate model/instance name
+  describe "#edit" do 
+
+  	#this sets up an insatnce of Item to be used in our tests
+    let(:item) {Item.create(first_attribute: "My name", second_attribute: 23)}
+
+    #this calls the #get action before each test
+    before(:each) do 
+      get :edit, id: item
+    end
+
+    #change "item"
+    it "finds a specific item" do 
+      expect(assigns(:item)).to eq(item)
+    end
+
+    #there should be nothing to change here unless you render a view other than edit
+    it "renders the edit view" do 
+      expect(response).to render_template("edit")
+    end
+  end
   
 end
